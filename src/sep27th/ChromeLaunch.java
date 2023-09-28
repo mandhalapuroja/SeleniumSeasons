ee7a6d7456bbbc96f973da04d8aa5f0826250f4f
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-09-28 08:31:52 +0530

    Selenium Season Programs till Sep28th

--- src/sep27th/ChromeLaunch.java
+++ src/sep27th/ChromeLaunch.java
@@ -1,13 +1,20 @@
 package sep27th;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ChromeLaunch {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "D:\\005 Live Tech\\jars\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "D:\\005 Live Tech\\jars\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
+		
+		//WebDriverManager.chromedriver().setup();
+		WebDriverManager.firefoxdriver().setup();
 		new ChromeDriver();
+		//new FirefoxDriver();
 
 	}
 
